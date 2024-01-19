d6103069e2aafc1963eb9c166540933f944408d3
FilipONT <filip.babiec@ont.com.pl>
2024-01-19 12:54:44 +0100

    Went back to previous files

--- functions.h
+++ functions.h
@@ -1,7 +1,7 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H 
 
-short func_overflowRed(short x);
+short func_overflow(short x);
 void func_BitShift(signed char x);
 void func_IDP_test();
 void func_NTL();
